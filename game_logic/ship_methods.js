1f59f0d91ebe8cb67ff294f518bde9125b93351e
NoraKitchen <narwhaltortellini@gmail.com>
2016-09-07 23:59:22 -0600

    works multi ships, multi locations

--- game_logic/ship_methods.js
+++ game_logic/ship_methods.js
@@ -1,19 +1,21 @@
 
 
-function checkForShip(player, coordinates){
-    for (var i = 0; i < player.ships.length; i++){
+function checkForShip(player, coordinates) {
+    for (var i = 0; i < player.ships.length; i++) {
+        //for every ship the player has placed...
+        //it looks at each coordinate that ship is on, and if it matches the coordinates, that coordinate becomes part of the shipPresent array returned
         var currentShip = player.ships[i];
-        var shipPresent = currentShip.locations.filter(function(playerShipCoordinates){
+        var shipPresent = currentShip.locations.filter(function (playerShipCoordinates) {
             //remember, .filter is run on an array and returns an array ---arg is a function that returns true or false--the resulting array is composed of all array items the test function returned true for
             return (playerShipCoordinates[0] === coordinates[0] && (playerShipCoordinates[1] === coordinates[1]));
             //so it will return any matches, thus if here are no matches, nothing will return
         })[0];
 
-        if (!shipPresent){
-            return false
+        if (shipPresent) {
+            return true
         }
     }
-
+    return false
 }
 
 module.exports.checkForShip = checkForShip;
--- test/shipTest.js
+++ test/shipTest.js
@@ -17,4 +17,62 @@ describe("check if ship already present", function () {
 
         expect(checkForShip(player, [9, 9])).to.be.false;
     })
+
+    it('should correctly report a ship is present at given location', function () {
+
+        //make an example player obj to test with
+        var player = {
+            ships: [
+                {
+                    locations: [[0, 0]]
+                }
+            ]
+        }
+
+        expect(checkForShip(player, [0, 0])).to.be.true;
+    })
+
+    it('should handle ships located at more than one coordinate', function () {
+
+        //make an example player obj to test with
+        var player = {
+            ships: [
+                {
+                    locations: [[0, 0], [0, 1]]
+                }
+            ]
+        }
+
+        //normally you don't have more than one expectation here cause it might be unclear what it's trying to prove
+        //but for brevity this one is decently clear?
+        expect(checkForShip(player, [0, 1])).to.be.true;
+        expect(checkForShip(player, [0, 0])).to.be.true;
+        expect(checkForShip(player, [9, 9])).to.be.false;
+    })
+
+    it('should handle checking multiple ships', function () {
+
+        //make an example player obj to test with
+        var player = {
+            ships: [
+                {
+                    locations: [[0, 0], [0, 1]]
+                },
+                {
+                    locations: [[1, 0], [1, 1]]
+                },
+                {
+                    locations: [[2, 0], [2, 1], [2, 2], [2, 3]]
+                }
+            ]
+        }
+
+        expect(checkForShip(player, [0, 1])).to.be.true;
+        expect(checkForShip(player, [0, 0])).to.be.true;
+        expect(checkForShip(player, [1, 0])).to.be.true;
+        expect(checkForShip(player, [1, 1])).to.be.true;
+        expect(checkForShip(player, [2, 3])).to.be.true;
+        expect(checkForShip(player, [9, 9])).to.be.false;
+    })
+
 })
